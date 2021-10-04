8c4a28bd5c9af4f230a1fc6f0c9fddb61ce6d618
syahirahshamshudin <sharifahsyahirah1351@gmail.com>
2021-10-04 11:53:59 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 03</title>
+</head>
+<body>
+    <h1>Asas PHP</h1>
+    <?php
+    // komen satu baris
+    /*komen banyak baris */
+    
+    $a = 'Syahirah';
+    echo "<h2>Hello $a</h2>"; #output
+
+    $hadir= false;
+
+    ?>
+</body>
+</html>
